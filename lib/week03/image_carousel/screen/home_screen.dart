f7d7305fdc4ff3e9124fac20e916ce4580758c61
lellon0403 <a67733198@gmail.com>
2025-09-28 21:57:29 +0900

    feat(image_carousel): Set BoxFit.cover to carousel images

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -10,8 +10,9 @@ class HomeScreen extends StatelessWidget {
         //map() 함수는 1.4.1 map()을 참조해주세요.
       children: [1,2,3,4,5] //샘플 리스트 생성
       .map( //위젯으로 매핑
-      (number) => Image.asset('asset/img/image_$number.jpeg'),
-      
+      (number) => Image.asset(
+        'asset/img/image_$number.jpeg'),
+        fit:BoxFit.cover, // BoxFit.cover 설정
       )
       .toList(),
       ),
