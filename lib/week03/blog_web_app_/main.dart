3c7d023b358fa7b0b8e151ba61494c6b59f2400f
lellon0403 <a67733198@gmail.com>
2025-09-28 19:20:51 +0900

    feat: Implement basic home screen setup for blog app

---
+++ lib/week03/blog_web_app_/main.dart
@@ -0,0 +1,10 @@
+import 'package:myapp/week03/blog_web_app_/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
