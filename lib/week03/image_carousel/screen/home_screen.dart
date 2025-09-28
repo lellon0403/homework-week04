a333c4953e2aea9a9302f5ab1137ca734329c534
lellon0403 <a67733198@gmail.com>
2025-09-28 22:23:31 +0900

    feat: Add PageController to Image Carousel

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -35,6 +35,8 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: PageView( //PageView 추가
         //map() 함수는 1.4.1 map()을 참조해주세요.
+      controller: pageController, //PageController 등록
+     
       children: [1,2,3,4,5] //샘플 리스트 생성
       .map( //위젯으로 매핑
       (number) => Image.asset(
