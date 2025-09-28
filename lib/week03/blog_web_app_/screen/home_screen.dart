65aca199ab0aec98295c97428aa49169cb467802
lellon0403 <a67733198@gmail.com>
2025-09-28 19:26:51 +0900

    feat: Initialize WebViewController in HomeScreen

--- lib/week03/blog_web_app_/screen/home_screen.dart
+++ lib/week03/blog_web_app_/screen/home_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-
+  WebViewController webViewController = WebViewController();
+  
   //const 생성자 
   const HomeScreen({Key? key}) : super(key : key);
   
