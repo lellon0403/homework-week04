3ad20207f7b26a5341fc09e4dcdb574fc2f9ac25
lellon0403 <a67733198@gmail.com>
2025-09-28 21:57:01 +0900

    feat(image_carousel): Add PageView with image assets

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -7,6 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView( //PageView 추가
+        //map() 함수는 1.4.1 map()을 참조해주세요.
       children: [1,2,3,4,5] //샘플 리스트 생성
       .map( //위젯으로 매핑
       (number) => Image.asset('asset/img/image_$number.jpeg'),
