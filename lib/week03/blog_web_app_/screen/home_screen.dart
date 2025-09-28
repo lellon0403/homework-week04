e285f0c6ecdbd5fc5e12cfd13d5c56eea1fe2e37
lellon0403 <a67733198@gmail.com>
2025-09-28 19:48:21 +0900

    feat(home_screen): Add home button to AppBar for blog navigation

--- lib/week03/blog_web_app_/screen/home_screen.dart
+++ lib/week03/blog_web_app_/screen/home_screen.dart
@@ -30,6 +30,23 @@ class HomeScreen extends StatelessWidget {
 
         //가운데 정렬
         centerTitle: true,
+
+        //AppBar에 액션 버튼을 추가할 수 있는 매개변수
+        actions: [
+          IconButton(
+
+            //아이콘을 눌렀을 때 실행할 콜백 함수
+            onPressed: (){
+              //웹뷰 위젯에서 사이트 전환하기
+              webViewController.loadRequest(Uri.parse('https://blog.codefactory.ai'));
+            },
+
+            // 홈 버튼 아이콘 설정
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+        ],
       ),
 
       
