a6148d3dbd791e45b72c727aa2b75e5c765b953d
lellon0403 <a67733198@gmail.com>
2025-09-28 19:19:21 +0900

    Fix: Remove redundant colon in HomeScreen constructor initializer.

--- lib/week03/blog_web_app_/screen/home_screen.dart
+++ lib/week03/blog_web_app_/screen/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
 
   //const 생성자 
-  const HomeScreen({Key? key} : super(key : key));
+  const HomeScreen({Key? key}) : super(key : key);
   
   @override
   Widget build(BuildContext context) {
