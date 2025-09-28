77122865ca9e978b801bead0e4073415df4fd5ed
lellon0403 <a67733198@gmail.com>
2025-09-28 19:18:21 +0900

    feat: Implement basic home screen layout

--- lib/week03/blog_web_app_/screen/home_screen.dart
+++ lib/week03/blog_web_app_/screen/home_screen.dart
@@ -6,5 +6,10 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key} : super(key : key);
   
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+  }
   )
 }
